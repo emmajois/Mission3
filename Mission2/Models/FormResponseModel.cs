8186bcb39e941873d572f0a12b386e35cc8ec036
Emma Gardner <emmajoig@gmail.com>
2022-01-17 19:27:07 -0700

    Mission 3 Changes

---
+++ Mission2/Models/FormResponseModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2.Models
+{
+    public class FormResponseModel
+    {
+        [Range(0, 100)]
+        public int Assignment { get; set; }
+
+        [Range(0, 100)]
+        public int Group { get; set; }
+
+        [Range(0, 100)]
+        public int Quiz { get; set; }
+
+        [Range(0, 100)]
+        public int Exam { get; set; }
+
+        [Range(0, 100)]
+        public int Intex { get; set; }
+
+    }
+}
